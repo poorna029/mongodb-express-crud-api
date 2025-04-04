2469c115bc7b4fe4158aa8c2937bd2a0bb898f2b
poorna029 <poorna029@gmail.com>
2025-04-04 15:18:53 +0530

    created http server using express

--- app.js
+++ app.js
@@ -1,71 +1,32 @@
-// why we are not using inbuilt http module
-//  is it is difficult to maintain let's compare it with express server 
+// express http server method 
+const express = require("express");
 
+const app = express(); 
 
-const http = require("http");
+// unlike http module for catching a route we require one if statement 
+// for catching http method another if statement
 
-const server = http.createServer((req , res)=>{
-    // any route/path will hit this callback function which very confusing to maintain all the code...
+// if any route is not found express handles it unlike http module way so
+// i am not showing other route here you can try it on your own
 
-
-    // if you want to handle routes we need to catch with if statements like this below 
-
-    if (req.url==="/")  
-    {
-        // if it is get method also we have to use if block , 
-        // so it is tedious task to manage every method on every route
-
-        if(req.method="GET"){
-            console.log("get request");
-        }else if(req.method ="POST"){
-            console.log("post request");
-        }else if(req.method ="PUT"){
-            console.log("put request");
-        }else if(req.method="PATCH"){
-            console.log("patch request");
-        }else if(req.method="DELETE"){
-            console.log("delete request");
-        }else{
-            console.log("invalid request");
-        }
-
-        res.write(" you are on home route...");
-        res.end();
-    }
-    else if (req.url==="/other") 
-    {
-        switch (req.method) {
-            case "GET":
-                console.log("get req");
-                break;
-            case "POST":
-                console.log("post req");
-                break;
-            case "PATCH":
-                console.log("patch req");
-                break;
-            case "PUT":
-                console.log("put req");
-                break;
-            case "DELETE":
-                console.log("delete req");
-                break;
-        
-            default:
-                console.log("invalid req method");
-                break;
-        }
-        res.write(" you are on other route ...");
-        res.end();
-    }
-    else 
-    {
-        res.write(" you are on anonymous route ...");
-        res.end();
-    }
+app.get("/" , (req,res,next)=>{ 
+    res.send({url:req.url,method: req.method})
 })
 
-server.listen(3000,()=>console.log(`server is running on port 3000 ...`));
-// so , this way either we have to use if blocks or switch cases to handle different methods on different routes 
+app.post("/" , (req,res,next)=>{
+    res.send({url:req.url,method: req.method})  
+})
 
+app.delete("/" , (req,res,next)=>{
+    res.send({url:req.url,method: req.method})
+})
 
+app.put("/" , (req,res,next)=>{
+    res.send({url:req.url,method: req.method})
+})
+
+app.patch("/" , (req,res,next)=>{
+    res.send({url:req.url,method: req.method})
+})
+
+app.listen(3000,()=>{console.log(`server running at portn 3000`)});
